33cb2f56190b5349341601c58f5fb2c7bcd6cde0
Milko Yanakiev <myanakiev@hotmail.com>
2020-03-07 10:51:53 +0200

    Stack

--- LinearDS/src/main/java/Main.java
+++ LinearDS/src/main/java/Main.java
@@ -3,25 +3,25 @@ import implementations.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        arr.add(0);
-        arr.add(1);
-        arr.add(2);
-
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i));
-        }
-
-
-
-        System.out.println("--------------------");
-
-        System.out.println("removed " + arr.remove(2));
-
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i));
-        }
+//        ArrayList<Integer> arr = new ArrayList<>();
+//
+//        arr.add(0);
+//        arr.add(1);
+//        arr.add(2);
+//
+//        for (int i = 0; i < arr.size(); i++) {
+//            System.out.println(arr.get(i));
+//        }
+//
+//
+//
+//        System.out.println("--------------------");
+//
+//        System.out.println("removed " + arr.remove(2));
+//
+//        for (int i = 0; i < arr.size(); i++) {
+//            System.out.println(arr.get(i));
+//        }
 
     }
 }
