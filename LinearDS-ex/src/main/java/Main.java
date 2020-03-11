7ffc95eab4f710c5c9f9981cac4b906f95b550e6
unknown <myanakiev@gmail.com>
2020-03-11 17:28:54 +0200

    ArrayDeque begin

--- LinearDS-ex/src/main/java/Main.java
+++ LinearDS-ex/src/main/java/Main.java
@@ -1,5 +1,11 @@
+import implementations.ArrayDeque;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayDeque<Integer> deque = new ArrayDeque();
+
+        deque.add(1);
 
     }
 }
--- LinearDS-ex/src/main/java/implementations/ArrayDeque.java
+++ LinearDS-ex/src/main/java/implementations/ArrayDeque.java
@@ -5,13 +5,59 @@ import interfaces.Deque;
 import java.util.Iterator;
 
 public class ArrayDeque<E> implements Deque<E> {
-    @Override
-    public void add(E Element) {
+    private final int INITIAL_CAPACITY = 7;
 
+    private int size;
+    private int head;
+    private int tail;
+
+    private Object[] elements;
+
+    public ArrayDeque() {
+        this.elements = new Object[INITIAL_CAPACITY];
+        int middle = INITIAL_CAPACITY / 2;
+        head = tail = middle;
+        this.size = 0;
+    }
+
+    @Override
+    public void add(E element) {
+        if(this.size == 0){
+            this.elements[this.tail] = element;
+        } else {
+            if(this.tail == this.elements.length - 1) {
+                this.elements = grow();
+            }
+            this.elements[++this.tail] = element;
+        }
+        this.size++;
+    }
+
+    private Object[] grow() {
+        int newCapacity = this.elements.length * 2 + 1;
+
+        Object[] newElements = new Object[newCapacity];
+
+        int middle = newCapacity / 2;
+
+        int begin = middle - this.size / 2;
+
+        int index = this.head;
+
+        for (int i = begin; index <= this.tail ; i++) {
+            newElements[i] = this.elements[index++];
+        }
+
+        this.head = begin;
+        this.tail = this.head + this.size - 1;
+
+        return newElements;
     }
 
     @Override
     public void offer(E element) {
+
+        // todo 2:19:38 https://youtu.be/Db4_Vk8TiKI?t=8378
 
     }
 
