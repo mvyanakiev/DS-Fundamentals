cd0d4b66a321509818382a885eeca85126c3d5d0
Milko Yanakiev <myanakiev@hotmail.com>
2020-03-19 13:01:11 +0200

    tree factory

--- Trees-ex/src/main/java/implementations/TreeFactory.java
+++ Trees-ex/src/main/java/implementations/TreeFactory.java
@@ -1,7 +1,10 @@
 package implementations;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+//https://youtu.be/ijMFkTV0HUI?t=3889
 
 public class TreeFactory {
     private Map<Integer, Tree<Integer>> nodesByKeys;
@@ -11,19 +14,39 @@ public class TreeFactory {
     }
 
     public Tree<Integer> createTreeFromStrings(String[] input) {
-        return null;
+        for (String params : input) {
+            int[] keys = Arrays.stream(params.split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            int parentKey = keys[0];
+            int childKey = keys[1];
+
+            this.addEdge(parentKey, childKey);
+        }
+        return this.getRoot();
     }
 
     private Tree<Integer> getRoot() {
+        for (Tree<Integer> value : nodesByKeys.values()) {
+            if(value.getParent() == null){
+                return value;
+            }
+        }
         return null;
     }
 
     public Tree<Integer> createNodeByKey(int key) {
-        return null;
+        this.nodesByKeys.putIfAbsent(key, new Tree<>(key));
+        return this.nodesByKeys.get(key);
     }
 
     public void addEdge(int parent, int child) {
+        Tree<Integer> parentByKey = this.createNodeByKey(parent);
+        Tree<Integer> childByKey = this.createNodeByKey(child);
 
+        childByKey.setParent(parentByKey);
+        parentByKey.addChild(childByKey);
     }
 }
 
