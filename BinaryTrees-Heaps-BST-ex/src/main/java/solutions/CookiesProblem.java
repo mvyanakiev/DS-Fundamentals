a3ea719ba633a01351f03996199d98c54e4b88ed
Milko Yanakiev <myanakiev@hotmail.com>
2020-03-28 15:28:14 +0200

    CookiesProblem

--- BinaryTrees-Heaps-BST-ex/src/main/java/solutions/CookiesProblem.java
+++ BinaryTrees-Heaps-BST-ex/src/main/java/solutions/CookiesProblem.java
@@ -1,7 +1,35 @@
 package solutions;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class CookiesProblem {
-    public Integer solve(int k, int[] cookies) {
-        return null;
+
+    public Integer solve(int requiredSweetness, int[] cookiesSweetness) {
+
+        Queue<Integer> cookies = new PriorityQueue<>();
+        int count = 0;
+
+        if (cookiesSweetness.length > 0) {
+            for (int sweetness : cookiesSweetness) {
+                cookies.add(sweetness);
+            }
+        } else {
+            count = -1;
+            return count;
+        }
+
+        int currentMinSweetness = cookies.peek();
+
+        while (currentMinSweetness < requiredSweetness && cookies.size() > 1) {
+            int least = cookies.poll();
+            int secondLeast = cookies.poll();
+            int combinedSweetness = least + 2 * secondLeast;
+            cookies.add(combinedSweetness);
+            currentMinSweetness = cookies.peek();
+            count++;
+        }
+
+        return currentMinSweetness > requiredSweetness ? count : -1;
     }
 }
